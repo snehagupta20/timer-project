c5128ef15fd556ddeb25c57b02650c84932ecc4d
sneha gupta <gsneha20012004@gmail.com>
2024-08-11 18:10:44 +0530

    first code using use refs. learned use refs. took input from user using use ref

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,29 @@
+import { useRef, useState } from "react";
+
 export default function Player() {
+
+  //to get val entered in input field
+  const playerName = useRef();
+
+  const [userName, setUserName] = useState("unknown entity");
+  // var name = "unknown entity";
+
+  const handleClick = () => {
+    // setUserName(name);
+    setUserName(playerName.current.value);
+  }
+
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {userName}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerName} type="text"/>
+        <button onClick={handleClick} >Set Name</button>
       </p>
     </section>
   );
 }
+
+// 1. store the input
+// 2. when button clicked, take the stored input and add in h2
